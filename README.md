59ec83904308451af4ae07b722734038fc0f22d9
jwalter <joakim.waltersson@gmail.com>
2016-06-11 17:41:21 +0200

    Now using local-links for internal navigation

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # eventor-js
-This is a readme file witout any proper content
+This is a readme file witout any proper content.
--- src/pages/public.js
+++ src/pages/public.js
@@ -1,12 +1,22 @@
 import React from 'react'
+import NavHelper from '../components/nav-helper'
 
 export default React.createClass({
   displayName: 'PublicPage',
+  
   render() {
     return (
-      <div className='container'>
-        <a href='/repos' className='button button-large'>Big button</a>
-      </div>
+      <NavHelper className='container'>
+        <header role='banner'>
+          <h1>Lablr</h1>
+        </header>
+        <div>
+          <p>We label stuff for you, because we can&trade;</p>
+          <a href='/repos' className='button button-large'>
+            <span className='mega-octicon octicon-mark-github'></span> Login with Github
+          </a>
+        </div>
+      </NavHelper>
     )
   }
 })
